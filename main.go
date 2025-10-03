176feb1405b4506f84ebb814e802c9846ca70aef
UndeadTokenArt <undeadtokenart@gmail.com>
2025-10-03 15:19:22 -0700

    adding re-route for the dm tools project

--- main.go
+++ main.go
@@ -10,10 +10,8 @@ import (
 func main() {
 
 	// run the server with the "run" tag present
-	if len(os.Args) > 1 && os.Args[1] == "-run" {
-		if len(os.Args) > 2 {
-			runServer()
-		}
+	if len(os.Args) > 1 && os.Args[1] == "run" {
+		runServer()
 	}
 
 	// --tagger filestart make a template from html file (also I know this is a teribble name)
